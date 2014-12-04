c3dfb8bbb26150c1f03d9b96b77f81edd6a779d2
thatknitter <kblewis.flt@gmail.com>
2014-12-04 15:06:07 -0600

    stuff in a function

--- js/main.js
+++ js/main.js
@@ -8,18 +8,12 @@
       name: 'Knit Shawl',
       desc: 'Finish that bad boy!',
       due: 'Friday'
-    },
-    {
-      name: 'Get Address',
-      desc: 'Get Swapners address',
-      due: 'Friday'
-    },
-    {
-      name: 'Get stuff',
-      desc: 'get stuff for swap box',
-      due: 'Friday'
     }
     ];
+    
+    vm.addNewTask = function(){
+      vm.tasks.push(vm.newTask);
+    };
   });
 
   }()); 
