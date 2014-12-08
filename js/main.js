0bf133c366d20f9c5d51b6c2959aa3332da87166
thatknitter <kblewis.flt@gmail.com>
2014-12-08 13:35:31 -0600

    sends to firebase

--- js/main.js
+++ js/main.js
@@ -1,29 +1,43 @@
 ;( function() {
     'use strict';
-  angular.module('todoApp', [])
-  .controller('TodoController', function(){
-    var vm = this;
-    vm.tasks = [
-    
-    ];
-    vm.info = [
-    
-    ];
-    
-    vm.addNewTask = function(){
-      vm.tasks.push(vm.newTask);
-      vm.newTask = null;
-    };
-    
-    vm.removeTodo = function(todo){
-      var index = vm.tasks.indexOf(todo);
-      vm.tasks.splice(index, 1);
-    };
-    
-    vm.addInfo = function(){
-      vm.info.push(vm.newInfo);
-      vm.newInfo = null;
-    };
-  });
+    angular.module('todoApp', []).controller('TodoController', function($http) {
+      var vm = this;
+      vm.tasks = [];
+      vm.info = [];
+      
+      $http.get('https://swaptodo.firebaseio.com/swaptodo.json')
+        .success(function(data){
+          vm.swaptodo = data;
+        })
+        .error(function(err){
+          console.log('well shit' + err);
+        });
 
-  }()); 
+      vm.addNewTask = function() {
+        $http.post(vm.swaptodothing, vm.newTask)
+        .success(function(data){
+          vm.swaptodotasks[data.name] = vm.newTask;
+        });
+        vm.tasks.push(vm.newTask);
+        vm.newTask = null;
+      };
+
+      vm.removeTodo = function(todo) {
+        var index = vm.tasks.indexOf(todo);
+        vm.tasks.splice(index, 1);
+      };
+
+      vm.addInfo = function() {
+        $http.post('https://swaptodo.firebaseio.com/swaptodo.json', vm.newInfo)
+        .success(function(data){
+          console.log(data);
+          console.log(vm.swaptodo);
+          vm.swaptodo[data.name] = vm.newInfo;
+          vm.swaptodothing = 'https://swaptodo.firebaseio.com/swaptodo/' + data.name + '.json';
+        });
+        vm.info.push(vm.newInfo);
+        
+      };
+    });
+
+  }());
